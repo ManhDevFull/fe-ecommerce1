b5eb1d34fd1031aab3f6a6fa31f2afe8b0e7e59b
Le Khac Hieu <lekhachieupt@gmail.com>
2025-12-01 18:00:55 +0700

    add model humster

--- src/app/(public)/(maketing)/page.tsx
+++ src/app/(public)/(maketing)/page.tsx
@@ -8,13 +8,14 @@ import { Features } from "@/components/templates/features/features";
 import Frequently from "@/components/templates/frequently/frequenty";
 import ShopByCategory from "@/components/templates/shopByCategory/ShopByCategory";
 import { Slider } from "@/components/templates/slider/slider";
-import React from "react";
+import React, { useState } from "react";
 
 type RootLayoutProps = {
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 };
 export default function Home({ children, params }: RootLayoutProps) {
+   const [isLoading, setIsLoading] = useState<boolean>(true);
   return (
     <div className="w-full p-0">
       <Slider/>
